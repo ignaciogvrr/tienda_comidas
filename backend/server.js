6566583f8befd64db0b93044cf436b26ede4cf74
Ignaciogvr <ign.nunezh@duocuc.cl>
2026-07-06 21:07:27 -0400

    Configuracion inicial EKS

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const {
-  DB_HOST = "10.0.2.134", // acá colocar la IP Privada EC2 DB
+  DB_HOST = "tienda-comidas-db", // resuelve internamente en EKS via DNS de Kubernetes
   DB_USER = "root",
   DB_PASSWORD = "admin123",
   DB_NAME = "tienda_perritos",
